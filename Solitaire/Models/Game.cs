13d31a3519e46786885af51267ba9257c218ab2e
Sam Plews <sam@codeite.com>
2014-09-26 14:55:14 +0100

    First failing approval test for inital board state

---
+++ Solitaire/Models/Game.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Solitaire.Models
+{
+    public class Game
+    {
+        public string Render()
+        {
+            return "Not Implimented";
+        }
+    }
+}
---
+++ Tests/ApprovalTests/GameStartupTest.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ApprovalTests;
+using ApprovalTests.Reporters;
+using NUnit.Framework;
+using Solitaire.Models;
+
+namespace Tests.ApprovalTests
+{
+    [TestFixture]
+    [UseReporter(typeof(DiffReporter))]
+    class GameStartupTest
+    {
+        [Test]
+        public void the_board_should_appear_correct_on_startup()
+        {
+            // When the game is started
+            var game = new Game();
+
+            // the board should be correct
+            string board = game.Render();
+            Approvals.Verify(board);
+        }
+    }
+}
