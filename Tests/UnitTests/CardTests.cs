6138b0df04d25bb4b8a83369c52a0b78bcd64f09
Sam Plews <sam@codeite.com>
2014-09-26 13:03:14 +0100

    First test for card

---
+++ Tests/UnitTests/CardTests.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+
+namespace Tests.UnitTests
+{
+    [TestFixture]
+    class CardTests
+    {
+        [Test]
+        public void back_of_card_should_be_blank()
+        {
+            dynamic card = new object();
+
+            var appearance = card.render();
+
+            appearance.Should.Be("**");
+        }
+    }
+}
