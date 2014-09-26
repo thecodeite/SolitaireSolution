2988d779e44fc109fe2838728ac5adf7908475c1
Sam Plews <sam@codeite.com>
2014-09-26 13:17:35 +0100

    Added faced ness to card

--- Solitaire/Models/Card.cs
+++ Solitaire/Models/Card.cs
@@ -8,6 +8,13 @@ namespace Solitaire.Models
 {
     public class Card
     {
+        public Card()
+        {
+            IsFaceDown = true;
+        }
+
+        public bool IsFaceDown { get; set; }
+
         public string Render()
         {
             return "**";
