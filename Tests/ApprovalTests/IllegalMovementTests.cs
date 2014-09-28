abb5768827ecdb41cd6704cf563c0955ac3b9b7d
Sam Plews <sam@codeite.com>
2014-09-28 20:28:28 +0100

    Finished adding negetive tests

--- Tests/ApprovalTests/IllegalMovementTests.cs
+++ Tests/ApprovalTests/IllegalMovementTests.cs
@@ -44,44 +44,79 @@ namespace Tests.ApprovalTests
 
         [Test]
         [Ignore]
-        public void can_make_legal_move_ace_from_column_to_waste()
+        // Cards may only be moved to a numbered column if the lowest card on it is of the same suit and is one higher
+        // Postponing writing test due to dubious specification.
+        public void can_make_illegal_move_of_card_on_top_of_same_suit()
         {
             var game = new Game();
             game.Deal(Deck.MakeTestDeck());
 
             // Move ace of hearts to hearts discard pile
+            //game.ParseInput("HA H");
+
+            string board = game.Render();
+            //Approvals.Verify(board);
+        }
+
+        [Test]
+        // Kings may be moved to columns that are empty
+        public void can_make_illigal_move_of_card_other_than_king_onto_empty_space()
+        {
+            var game = new Game();
+            game.Deal(Deck.MakeTestDeck());
+
+            // Move ace of hearts to hearts discard pile to make a space
             game.ParseInput("HA H");
+
+            // Move eight of diamonds to column 1
+            game.ParseInput("D8 1");
 
             string board = game.Render();
             Approvals.Verify(board);
         }
 
         [Test]
-        [Ignore]
-        public void can_make_legal_move_king_to_empty_space()
+        // Aces may be moved to the D,H,C,S discard piles if they are empty
+        public void can_make_illigal_move_of_card_other_than_ace_onto_empty_discard_pile()
         {
             var game = new Game();
             game.Deal(Deck.MakeTestDeck());
 
-            // Move ace of hearts to hearts discard pile
-            game.ParseInput("HA H");
-
-            // Move king of X to column 1
-            game.ParseInput("sK 1");
+            // Move eight of diamonds from stack to hearts discard pile
+            game.ParseInput("D8 D");
 
             string board = game.Render();
             Approvals.Verify(board);
         }
 
         [Test]
-        [Ignore]
-        public void can_make_legal_move_card_from_stack_to_columns()
+        // Aces may be moved to the D,H,C,S discard piles if they are empty
+        public void can_make_illigal_move_of_card_of_wrong_suit_to_discard_pile()
         {
             var game = new Game();
             game.Deal(Deck.MakeTestDeck());
 
-            // Move two of diamonds from stack to column 4
-            game.ParseInput("D2 4");
+            // Move ace of hearts from stack to diamonds discard pile
+            game.ParseInput("HA D");
+
+            string board = game.Render();
+            Approvals.Verify(board);
+        }
+
+        [Test]
+        // Cards may only be moved to a D,H,C,S dicard pile if the lowest card on it is of the same suit and is one lower
+        public void can_make_illegal_move_of_non_sequential_card_to_discard_pile()
+        {
+            var game = new Game();
+            game.Deal(Deck.MakeTestDeck());
+
+            // Move ace of hearts from stack to hearts discard pile
+            game.ParseInput("HA H");
+
+            // Move the jack out of the way
+            game.ParseInput("cJ 7");
+
+            game.ParseInput("HJ H");
 
             string board = game.Render();
             Approvals.Verify(board);
